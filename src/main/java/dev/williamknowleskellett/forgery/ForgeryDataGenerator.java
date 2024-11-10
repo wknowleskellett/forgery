084a9435714968f0659df8bf86694093b4023885
William Knowles-Kellett <39969985+wknowleskellett@users.noreply.github.com>
2024-11-09 22:40:26 -0500

    Initialized from template repository

---
+++ src/main/java/dev/williamknowleskellett/forgery/ForgeryDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.williamknowleskellett.forgery;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ForgeryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
